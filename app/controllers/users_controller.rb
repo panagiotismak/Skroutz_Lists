f78974723b8f57ed56ee1477b39e927fa9d231bc
panagiotis <panagiotis.makropoulos@gmail.com>
2017-07-11 15:21:49 +0300

    edit, show, update user premachure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
+
   def new
    @user = User.new
   end
@@ -8,14 +10,35 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Skroutz Lists #{@user.username}"
-      redirect_to root_path
+      redirect_to user_path(@user)
   	else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "User was successfully updated"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    
+  end
+
   private
   def user_params  
   	params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
