9767b4d4e7d5d104bbf29beeab2a0eb702115b1d
panagiotis <panagiotis.makropoulos@gmail.com>
2017-07-16 16:46:29 +0300

    Login and user creation but not completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] =@user.id
       flash[:success] = "Welcome to Skroutz Lists #{@user.username}"
       redirect_to user_path(@user)
   	else
