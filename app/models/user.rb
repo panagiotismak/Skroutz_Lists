10c72872bf477e6be1f72ef81e4df15c3817f93d
panagiotis <panagiotis.makropoulos@gmail.com>
2017-07-16 18:11:46 +0300

    assigned the users/new as homepage and changed layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximun: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: { case_sensitive: false}, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+
+  before_save { self.email = email.downcase }
+
   has_secure_password
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximun: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false}, 
+            length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum:8, maximum: 100}
 end	
