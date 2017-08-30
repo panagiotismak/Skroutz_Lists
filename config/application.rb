d1e09c981a6ff5000d95d6fb2680df88af904029
panagiotis <panagiotis.makropoulos@gmail.com>
2017-08-30 17:03:55 +0300

    Revert "Eager load lib at config"
    This reverts commit 740d96c601010b7008548a7b29ea567ea90db1ed.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module SkroutzLists
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths += %W(#{config.root}/lib)
-    config.eager_load_paths += %w(#{config.root}/lib/skroutzapi)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
